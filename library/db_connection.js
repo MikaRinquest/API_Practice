8b49499eb702acdb673308e74f0a20f4cd8894db
MikaRinquest <102951738+MikaRinquest@users.noreply.github.com>
2022-08-02 16:09:41 +0200

    Update db_connection.js

--- library/db_connection.js
+++ library/db_connection.js
@@ -3,10 +3,10 @@ const mysql = require("mysql");
 require('dotenv').config()
 // Here to is use across multiple files. Used to make SQL queries to DB
 const con = mysql.createConnection({
-  host: process.env..DB_HOST,
-  user: process.env..DB_USER,
-  password: process.env..DB_PASS,
-  database: process.env..DB_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   multipleStatements: true,
 });
 
