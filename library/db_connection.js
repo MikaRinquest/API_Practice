52a92c16a097585082d7b212fa4e1719626d56f7
MikaRinquest <102951738+MikaRinquest@users.noreply.github.com>
2022-08-02 16:09:24 +0200

    Update db_connection.js

--- library/db_connection.js
+++ library/db_connection.js
@@ -1,13 +1,12 @@
 // This file will create a connection to the database
 const mysql = require("mysql");
-const creds = require("../config/creds");
-
+require('dotenv').config()
 // Here to is use across multiple files. Used to make SQL queries to DB
 const con = mysql.createConnection({
-  host: creds.DB_HOST,
-  user: creds.DB_USER,
-  password: creds.DB_PASS,
-  database: creds.DB_NAME,
+  host: process.env..DB_HOST,
+  user: process.env..DB_USER,
+  password: process.env..DB_PASS,
+  database: process.env..DB_NAME,
   multipleStatements: true,
 });
 
