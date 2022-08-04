8bf6de689d29ffb10f33f5d51a5aebe6cde89473
Mika Rinquest <rinquestmika@gmail.com>
2022-08-04 09:27:46 +0200

    fixed login

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,11 +1,12 @@
 function login() {
   const email = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
-  const token = fetch("http://localhost:6969/users/login", {
+  // const token =
+  fetch("http://localhost:6969/users/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "x-auth-token": token,
+      //   "x-auth-token": token,
     },
     body: JSON.stringify({
       email: email,
